924a240085d4ecac0330148c3b112b585b288b05
JakubDob <devjakubd@gmail.com>
2024-02-13 17:55:00 +0100

    Added package namespace, changed interfaces to types

--- src/lib/ngx-canvas-grid.types.ts
+++ src/lib/ngx-canvas-grid.types.ts
@@ -1,40 +1,40 @@
-export interface RenderTextParams {
+export type RenderTextParams = {
   text: string;
   font: string;
   color: string;
   cellRect: Rect;
-}
+};
 
-export interface Rect {
+export type Rect = {
   x: number;
   y: number;
   w: number;
   h: number;
-}
+};
 
-export interface Point2D {
+export type Point2D = {
   x: number;
   y: number;
-}
+};
 
-export interface CanvasGridCellRenderParams {
+export type CanvasGridCellRenderParams = {
   context: CanvasRenderingContext2D;
   renderTextFn: (params: RenderTextParams) => void;
   cellIndex: number;
   cellRect: Rect;
   deltaTime: number;
   elapsedTime: number;
-}
+};
 
-export interface GridDragEvent {
+export type GridDragEvent = {
   from: number;
   to: number;
-}
+};
 
-export interface GridClickEvent {
+export type GridClickEvent = {
   index: number;
   buttonId: number;
-}
+};
 
 export type CanvasGridCellRenderFn = (
   params: CanvasGridCellRenderParams
